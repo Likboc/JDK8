eff86f7181486f0b6d9afe123d949d3502aed1dc
downloader <git@github.com:Likboc/min-downloader.git>
2024-01-19 01:30:43 +0800

    init

--- src/test/Application.java
+++ src/test/Application.java
@@ -3,5 +3,6 @@ package test;
 public class Application {
     public static void main(String[] args) {
         DoorEvent a = new DoorEvent(1);
+
     }
 }
--- src/test/DoorEvent.java
+++ src/test/DoorEvent.java
@@ -3,12 +3,6 @@ package test;
 import java.util.EventObject;
 
 public class DoorEvent extends EventObject {
-    /**
-     * Constructs a prototypical Event.
-     *
-     * @param source The object on which the Event initially occurred.
-     * @throws IllegalArgumentException if source is null.
-     */
     public DoorEvent(Object source) {
         super(source);
     }
