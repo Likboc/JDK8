374730e04c7ec2b95cd9fa0fb033eedbdbf7578e
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-25 10:50:32 +0800

    添加 “hash函数”

--- src/collection/support/HashMap.java
+++ src/collection/support/HashMap.java
@@ -1,21 +1,22 @@
 package collection.support;
 
-import java.util.Map;
-import java.util.Objects;
-
+/**
+ * 基于数组 + 链表/TreeNode 生成经典 map结构
+ * 解决hash冲突方法：
+ * 1.链表
+ * 2.红黑树
+ * 扩容机制省略。
+ * @param <K>
+ * @param <V>
+ */
 public class HashMap<K,V> {
-    static final int Default_CAPACITY = 1 >> 4;
-
-    static final int MAXIMUM_CAPACITY = 1 << 30;
-
-    static final float DEFAULT_LOADER_FACTOR = 0.75f;
 
     /**
      * bucket is a good name
      */
-    transient Node<K,V>[] table;
+    private Node<K,V>[] table;
 
-    static class Node<K,V>  implements Map.Entry<K,V> {
+    static class Node<K,V> {
         final K key;
         final int hash;
         V value;
@@ -26,71 +27,38 @@ public class HashMap<K,V> {
             this.value = value;
             this.next = next;
         }
-
-        @Override
-        public K getKey() {
-            return key;
-        }
-
-        @Override
-        public V getValue() {
-            return value;
-        }
-
-        @Override
-        public V setValue(V value) {
-            V oldValue = value;
-            this.value = value;
-            return oldValue;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hashCode(key) * Objects.hashCode(value);
-        }
-
-        public final boolean equals(Object o) {
-            if (o == this)
-                return true;
-
-            return o instanceof Map.Entry<?, ?> e
-                    && Objects.equals(key, e.getKey())
-                    && Objects.equals(value, e.getValue());
-        }
     }
+
+
 
     public V get(K key) {
         int hash = key.hashCode() ^ (key.hashCode() >> 16);
         return null;
     }
 
+    /**
+     * 使用& 代替 % 获取索引
+     * @param key
+     * @param value
+     */
+    public void put(K key,V value) {
+        int index = hash(key) & (table.length - 1);
+        Node node = new Node<>(index,key,value,null);
+        if(table[index] != null) {
+            table[index].next = node;
+        }else{
+            table[index] = node;
+        }
+    }
+
+    /**
+     * 进行高低16位异或，使得hash分布更均匀
+     * @param key
+     * @return
+     */
     static final int hash(Object key) {
         int h;
         return (key == null) ? 0 :(h = key.hashCode()) ^ (h >>> 16);
     }
 
-//    public Node<K,V> getNode(Object key) {
-//
-//         Node<K,V>[] tab; Node<K,V> first, e; int n, hash; K k;
-//        if ((tab = table) != null && (n = tab.length) > 0 &&
-//            (first = tab[(n - 1) & (hash = hash(key))]) != null) {
-//            if (first.hash == hash && // always check first node
-//                ((k = first.key) == key || (key != null && key.equals(k))))
-//                return first;
-//            if ((e = first.next) != null) {
-//                if (first instanceof TreeNode)
-//                    return ((TreeNode<K,V>)first).getTreeNode(hash, key);
-//                do {
-//                    if (e.hash == hash &&
-//                        ((k = e.key) == key || (key != null && key.equals(k))))
-//                        return e;
-//                } while ((e = e.next) != null);
-//            }
-//        }
-//        return null;
-//    }
-
-//    final V putVal() {
-//        // 1.hash bucket 为空，直接放入
-//    }
 }
