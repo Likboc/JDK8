61a36eaf99dfd7017f35125919afb145c6414fad
downloader <git@github.com:Likboc/min-downloader.git>
2024-02-25 08:55:28 +0800

    添加 ”扩容机制“

--- src/collection/support/ArrayList.java
+++ src/collection/support/ArrayList.java
@@ -1,5 +1,7 @@
 package collection.support;
 
+import java.util.Arrays;
+
 /**
  * 特点 :
  * 1. 自动扩容
@@ -7,7 +9,7 @@ package collection.support;
  * @param <E>
  */
 public class ArrayList<E>  {
-    public static final Object[] tmp = {};
+    public static  Object[] tmp = {};
 
     public static final Object[] DEFAULTCAPACITY_EMPTY_ELEMENTDATA = {};
 
@@ -18,4 +20,49 @@ public class ArrayList<E>  {
     // 版本号
     protected transient int modCount = 0;
 
+    private double LoadFactor = 0.75d;
+
+
+    private int count = 0;
+    // add
+    public void add(E element) {
+        tmp[count] = element;
+        count++;
+    }
+    // 删除，涉及到元素的移动
+    public void remove(E element) {
+        for(int i = 0; i < count; i++) {
+            if(tmp[i] == element) {
+                while(i < count) {
+                    tmp[i] = tmp[i + 1];
+                    i++;
+                }
+                break;
+            }
+        }
+    }
+    public void update(E element) {
+        for(int i = 0; i < count; i++) {
+            if(tmp[i].equals(element)) {
+                tmp[i] = element;
+            }
+        }
+    }
+
+    // 扩容
+    public void kuorong() {
+        Object[] old = tmp;
+        Object[] newArr = Arrays.copyOf(tmp,size * 2);
+        tmp = newArr;
+        size = size << 2;
+    }
+
+    public boolean check() {
+        if(tmp.length > Math.floor(size * LoadFactor)) {
+            kuorong();
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
